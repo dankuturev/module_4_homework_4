2a1129ec4bcb426a0e9bd94a91f2fdbff829db48
Danila <dan.kutyrev@yandex.ru>
2022-05-15 09:16:27 +0300

    modified task1 and task4

--- task_1.py
+++ task_1.py
@@ -5,24 +5,24 @@ class FlatIterator:
 
     def __init__(self, nested_list):
         self.nested_list = nested_list
+        self.new_index = 0
+        self.len_new_list = len(self.flat_list())
 
     def flat_list(self):
         new_list = []
-        _index = 0
-        while _index < len(self.nested_list):
-            item = self.nested_list[_index]
+        index = 0
+        while index < len(self.nested_list):
+            item = self.nested_list[index]
             if isinstance(item, list):
                 item_index = 0
                 while item_index < len(item):
                     i = item[item_index]
                     new_list.append(i)
                     item_index += 1
-                _index += 1
+                index += 1
         return new_list
 
     def __iter__(self):
-        self.new_index = 0
-        self.len_new_list = len(self.flat_list())
         return self
 
     def __next__(self):
--- task_4.py
+++ task_4.py
@@ -1,11 +1,12 @@
 from arguments import nested_list
-from iteration_utilities import deepflatten
 
 
 def flat_generator(input_list):
-    new_list = list(deepflatten(input_list))
-    for i in new_list:
-        yield i
+    for item in input_list:
+        if isinstance(item, list):
+            yield from flat_generator(item)
+        else:
+            yield item
 
 
 if __name__ == '__main__':
@@ -13,13 +14,3 @@ if __name__ == '__main__':
         print(item)
     flat_list = [item for item in flat_generator(nested_list)]
     print(flat_list)
-
-
-
-
-
-
-
-
-
-
